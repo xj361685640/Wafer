8f2b34aa1439a07613be346e7f26dc6d5905aeff
Travis CI User <travis@example.org>
2018-06-17 16:54:17 +0000

    Documentation upload

---
+++ implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["wafer"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">__Deref</a> for <a class=\"struct\" href=\"wafer/output/struct.PROJDATE.html\" title=\"struct wafer::output::PROJDATE\">PROJDATE</a>",synthetic:false,types:[]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">__Deref</a> for <a class=\"struct\" href=\"wafer/output/struct.TERMWIDTH.html\" title=\"struct wafer::output::TERMWIDTH\">TERMWIDTH</a>",synthetic:false,types:["wafer::output::TERMWIDTH"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"wafer/errors/struct.Error.html\" title=\"struct wafer::errors::Error\">Error</a>",synthetic:false,types:["wafer::errors::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
